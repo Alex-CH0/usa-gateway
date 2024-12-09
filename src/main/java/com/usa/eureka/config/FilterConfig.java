fc317d25f1e4a8115995341d5901e8df69b23c65
Alex-CHO <hyungjin98@gmail.com>
2024-12-09 12:38:37 +0900

    bug fix of yml file, added custom fiter

---
+++ src/main/java/com/usa/eureka/config/FilterConfig.java
@@ -0,0 +1,19 @@
+package com.usa.eureka.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FilterConfig {
+/*    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r -> r.path("/member-service/**")
+                        .uri("lb://MEMBER-SERVICE"))
+                .route(r -> r.path("/order-service/**")
+                        .uri("lb://ORDER-SERVICE"))
+                .build();
+    }*/
+}
